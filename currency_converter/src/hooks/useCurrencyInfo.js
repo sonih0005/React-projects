4882285c6a542ef3d29dc535ea91b3a852ce273b
sonih0005 <sonih0005@gmail.com>
2024-05-08 22:17:27 +0530

    minor changes and bug fixes

--- currency_converter/src/hooks/useCurrencyInfo.js
+++ currency_converter/src/hooks/useCurrencyInfo.js
@@ -10,11 +10,13 @@ function useCurrencyInfo(currency){
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.min.json`)
         .then((res) => res.json())
         .then((res) =>  setData(res[currency]))
-        console.log(data)
+        
     },[currency])
 
-    console.log(data)
+   
     return data
 }
 
 export default useCurrencyInfo;
+
+
