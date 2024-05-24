bf6ae98c67d871eb99a9b9e82d48d5c7352a489b
sonih0005 <sonih0005@gmail.com>
2024-05-24 23:33:20 +0530

    Add functionality

--- ReduxRTK/src/App.jsx
+++ ReduxRTK/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import AddTodo from './Components/AddTodo'
+import Todos from './Components/Todos'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1>Redux ToolKit</h1>
+      <AddTodo />
+      <Todos />
     </>
   )
 }
